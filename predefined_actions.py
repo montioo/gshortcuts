500c682a51eb1509fa89a547a9fff3c72ae52327
Marius Montebaur <marius@montebaur.tech>
2020-11-29 23:14:42 +0100

    Added function to auto-hide the dock

--- predefined_actions.py
+++ predefined_actions.py
@@ -25,3 +25,12 @@ def disable_launch_terminal():
     """Unbinds <Ctrl><Alt>t from launching a terminal window."""
     cmd = 'gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "[]"'
     subprocess.call(["/bin/bash", "-c", cmd])
+
+def enable_dock_autohide():
+    """Hides the dock automatically to make more room for windows."""
+    cmds = [
+        "gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false"
+        "gsettings set org.gnome.shell.extensions.dash-to-dock autohide true"
+    ]
+    for cmd in cmds:
+        subprocess.call(["/bin/bash", "-c", cmd])
